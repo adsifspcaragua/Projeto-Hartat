c3617061ce2596c2a4a8abc6fce1f21cd33bef45
rafaelknji <rafael.obara10@outlook.com>
2026-05-16 01:23:22 -0300

    Implemented acervo page

--- src/components/PageRouter.jsx
+++ src/components/PageRouter.jsx
@@ -2,18 +2,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './css/Sidebar.css'
 import Home from '../pages/Home'
 import Exposicoes from '../pages/Exposicoes'
+import Acervo from "../pages/Acervo";
 
 function PageRouter() {
 
     return(
-        <main className='padding-wrapper'>
-            <BrowserRouter>
-                <Routes>
-                    <Route index element={<Home />}/>
-                    <Route path='/exposicoes' element={<Exposicoes />}/>
-                </Routes>
-            </BrowserRouter>
-        </main>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home />}/>
+                <Route path="/acervo" element={<Acervo />} />
+                <Route path='/exposicoes' element={<Exposicoes />}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
