5710eda574c98c5f1c51888395e9af2386364870
rafaelknji <rafael.obara10@outlook.com>
2026-05-16 01:23:22 -0300

    Implemented acervo page

--- src/components/PageRouter.jsx
+++ src/components/PageRouter.jsx
@@ -3,19 +3,19 @@ import './css/Sidebar.css'
 import Home from '../pages/Home'
 import Externo from '../pages/Externo'
 import Exposicoes from '../pages/Exposicoes'
+import Acervo from "../pages/Acervo";
 
 function PageRouter() {
 
     return(
-        <main className='padding-wrapper'>
-            <BrowserRouter>
-                <Routes>
-                    <Route index element={<Home />}/>
-                    <Route path="/atividades-externas" element={<Externo />}/>.
-                    <Route path='/exposicoes' element={<Exposicoes />}/>
-                </Routes>
-            </BrowserRouter>
-        </main>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home />}/>
+                <Route path="/acervo" element={<Acervo />} />
+                <Route path="/atividades-externas" element={<Externo />}/>
+                <Route path='/exposicoes' element={<Exposicoes />}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
