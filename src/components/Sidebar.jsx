76381d0d622c462c3575cd64f9ba5ac75ea627bd
KesleyMarco <kesleyreis07@gmail.com>
2026-05-20 15:30:56 -0300

    estrutura da sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,30 @@
 import './css/Sidebar.css'
+import logoHartat from '../assets/logo_hartat.webp';
 
 function Sidebar() {
 
     return(
-        <aside className="sidebar">
-            <h2>Sidebar</h2>
+        <aside className="sidebar"> 
+            <div className="sidebar_right">
+                <div className="sidebar_logo"><img src={logoHartat} alt="Logo"/></div>
+                <div className="sidebar_menu">
+                    <button className="sidebar_menu_item">Inicio</button>
+                    <button className="sidebar_menu_item">Sobre</button>
+                    <button className="sidebar_menu_item">Programação</button>
+                    <button className="sidebar_menu_item">Projetos</button>
+                    <button className="sidebar_menu_item">Acervo</button>
+                    <button className="sidebar_menu_item">Cine Hartat</button>
+                    <button className="sidebar_menu_item">Intercambios</button>     
+                </div>
+                <div className="sidebar_icons">
+                    <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                </div>
+                <div className="sidebar_info">lorem ipsum dolor sit amet</div>      
+            </div>
+            <div className="sidebar_left"></div>
         </aside>
     )
 }
