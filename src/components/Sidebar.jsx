54e6457717ee0ae6e741e7bbfafa0843c0af2ad4
KesleyMarco <kesleyreis07@gmail.com>
2026-05-26 22:18:56 -0300

    correção da posição da logo, aumento da sidebar, definiu o local do detalhe lateral, menu de opções estilizado e posicionado, conteúdo do "info"

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,27 +4,38 @@ import logoHartat from '../assets/logo_hartat.webp';
 function Sidebar() {
 
     return(
-        <aside className="sidebar"> 
-            <div className="sidebar_left">
-                <div className="sidebar_logo"><img src={logoHartat} alt="Logo" className="logo_hartat"/></div>
-                <div className="sidebar_menu">
-                    <button className="sidebar_menu_item">Inicio</button>
-                    <button className="sidebar_menu_item">Sobre</button>
-                    <button className="sidebar_menu_item">Programação</button>
-                    <button className="sidebar_menu_item">Projetos</button>
-                    <button className="sidebar_menu_item">Acervo</button>
-                    <button className="sidebar_menu_item">Cine Hartat</button>
-                    <button className="sidebar_menu_item">Intercambios</button>     
+        <aside className="sidebar"> {/* Container principal da sidebar */}
+            <div className="sidebar_left"> {/* Parte esquerda da sidebar, onde fica o conteúdo principal */}
+                <div className="sidebar_logo"> {/* Área reservada para a logo */}
+                    <img src={logoHartat} alt="Logo" className="logo_hartat"/>{/* Imagem da logo */}
+                </div> 
+                <div className="sidebar_menu"> {/* Área do menu de navegação */}
+                    <button className="sidebar_menu_item">Inicio <span>{">"}</span></button>
+                        <div className="separador"/>{/* Linha separadora entre itens */}
+                    <button className="sidebar_menu_item">Sobre <span>{">"}</span></button>
+                        <div className="separador"/>{/* Linha separadora entre itens */}
+                    <button className="sidebar_menu_item">Programação <span>{">"}</span></button>
+                        <div className="separador"/>{/* Linha separadora entre itens */}
+                    <button className="sidebar_menu_item">Projetos <span>{">"}</span></button>
+                        <div className="separador"/>{/* Linha separadora entre itens */}
+                    <button className="sidebar_menu_item">Acervo <span>{">"}</span></button>
+                        <div className="separador"/>{/* Linha separadora entre itens */}
+                    <button className="sidebar_menu_item">Cine Hartat <span>{">"}</span></button>
+                        <div className="separador"/>{/* Linha separadora entre itens */}
+                    <button className="sidebar_menu_item">Intercambios <span>{">"}</span></button>     
                 </div>
-                <div className="sidebar_icons">
+                <div className="sidebar_icons">{/* Área dos ícones/redes sociais */}
                     <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
                     <a href="#"><i class="fa-brands fa-facebook"></i></a>
                     <a href="#"><i class="fa-brands fa-instagram"></i></a>
                     <a href="#"><i class="fa-brands fa-twitter"></i></a>
                 </div>
-                <div className="sidebar_info">lorem ipsum dolor sit amet</div>      
+                <div className="sidebar_info">Rua Bartolomeu Bueno da Silva, 1231 - Martim de Sá
+                Caraguatatuba, SP 
+                +55 (12) 98195-0786
+                </div>{/* Texto/rodapé informativo da sidebar */}      
             </div>
-            <div className="sidebar_right"></div>
+            <div className="sidebar_right"></div> {/* Parte direita da sidebar, decoração */}
         </aside>
     )
 }
