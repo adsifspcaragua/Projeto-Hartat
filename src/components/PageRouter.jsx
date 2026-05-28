a101ccf45247625250b2642ed2ebde9a004336af
Mateus Fonseca <mateusfonsecasouza41@gmail.com>
2026-05-28 17:44:05 -0300

    Implemented exhibitions page

--- src/components/PageRouter.jsx
+++ src/components/PageRouter.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './css/Sidebar.css'
 import Home from '../pages/Home'
+import Externo from '../pages/Externo'
+import Exposicoes from '../pages/Exposicoes'
 
 function PageRouter() {
 
@@ -9,6 +11,8 @@ function PageRouter() {
             <BrowserRouter>
                 <Routes>
                     <Route index element={<Home />}/>
+                    <Route path="/atividades-externas" element={<Externo />}/>.
+                    <Route path='/exposicoes' element={<Exposicoes />}/>
                 </Routes>
             </BrowserRouter>
         </main>
